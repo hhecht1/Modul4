9e84262cf36a3aa0b42d3349434b270f71a070a6
Helge_ <helgehecht92@gmail.com>
2026-07-07 15:34:01 +0200

    "Update"

--- ConsoleChat/obj/Debug/net10.0/ConsoleChat.AssemblyInfo.cs
+++ ConsoleChat/obj/Debug/net10.0/ConsoleChat.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleChat")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f4abf7c6aa776be4b0b620a51bf67e1250054079")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b471108c024ab4041e7942a5e68dff49c6facf5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleChat")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleChat")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- FirstGameStore.Api/obj/Debug/net10.0/FirstGameStore.Api.AssemblyInfo.cs
+++ FirstGameStore.Api/obj/Debug/net10.0/FirstGameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstGameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d85ed2a46408b828667dac21d5e374b182de75b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b471108c024ab4041e7942a5e68dff49c6facf5")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstGameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstGameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
