fb1b17576fb9540df1242c64f1565a35e0b65d7e
Helge_ <helgehecht92@gmail.com>
2026-07-13 09:35:44 +0200

    NotificationService

--- FirstGameStore.Api/obj/Debug/net10.0/FirstGameStore.Api.AssemblyInfo.cs
+++ FirstGameStore.Api/obj/Debug/net10.0/FirstGameStore.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FirstGameStore.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a65c35460765ac03c0457e77d68443a3adf8ae3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ad95d6d8b0fc970399ed411552e56a374468d38b")]
 [assembly: System.Reflection.AssemblyProductAttribute("FirstGameStore.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FirstGameStore.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ NotifactionService/Program.cs
@@ -0,0 +1,70 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+
+namespace Notify
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            NotificationService emailService = new NotificationService(new EmailSender());
+            emailService.NotifyUser("Alice", "Deine Registrieung war erfolgreich!");
+
+
+            NotificationService smsService = new NotificationService(new SmSSender());
+            smsService.NotifyUser("+435676998533", "Deine Verfizierungscode lautet: 123456");
+
+            Console.ReadKey();
+
+
+        }
+    }
+
+    // 2. Definition des Interfaces IMessageService 
+    public interface IMessageService
+    {
+        void SendMessage(string recipient, string message);
+    }
+
+    // Konkrete Implementations of IMessageService
+    public class EmailSender : IMessageService
+    {
+        public void SendMessage(string recipient, string message)
+        {
+            Console.WriteLine($"Sending Email to {recipient} with message: {message}");
+        }
+    }
+
+    public class SmSSender : IMessageService
+    {
+        public void SendMessage(string recipient, string message)
+        {
+            Console.WriteLine($"Sending SMS to {recipient} with message: {message}");
+        }
+    }
+
+    // 3. Implementierung der Factory-Klasse , Der Service nutzt nun das Interface (Dependency Injection) und ist somit flexibel und erweiterbar.
+
+    public class NotificationService
+    {
+        private IMessageService _sender;
+
+        // Konstruktor, der eine IMessageService-Implementierung entgegennimmt
+        public NotificationService(IMessageService sender)
+        {
+            _sender = sender;
+        }
+
+        public void NotifyUser(string user, string msg)
+        {
+            string formattedMessage = $"Hallo {user}, {msg}";
+            _sender.SendMessage(user, formattedMessage);
+        }
+
+
+    }
+
+}
